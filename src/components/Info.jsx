f6253930c4593febdb21f053d68cc72875a6fefb
ChaminduSenanayake <78223836+ChaminduSenanayake@users.noreply.github.com>
2021-06-28 12:00:08 +0530

    Add files via upload

---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script and React.Js</h1>
+      <p>Basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
